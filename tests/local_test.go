987d31ceb6f30ae633ba157f0917c8f9cbbf7970
Abhinav Dahiya <abhinav.dahiya@coreos.com>
2017-06-09 14:08:30 -0700

    added some tests
    Signed-off-by: Abhinav Dahiya <abhinav.dahiya@coreos.com>

---
+++ tests/local_test.go
@@ -0,0 +1,57 @@
+package tests
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"testing"
+)
+
+func TestLocalOutput(t *testing.T) {
+	t.Run("Pod", testPod)
+	t.Run("Service", testService)
+}
+
+func testPod(t *testing.T) {
+	dir, err := ioutil.TempDir("", "pod")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := cr.WithLocalOuput(dir); err != nil {
+		t.Fatalf("error init local output %v", err)
+	}
+
+	results, err := cr.CollectPodLogs("kube-apiserver")
+	if err != nil {
+		t.Fatalf("error in collecting %v", err)
+	}
+
+	if len(results) == 0 {
+		t.Fatalf("alteast one container exists but not found")
+	}
+	t.Log(results)
+
+	os.RemoveAll(dir)
+}
+
+func testService(t *testing.T) {
+	dir, err := ioutil.TempDir("", "pod")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := cr.WithLocalOuput(dir); err != nil {
+		t.Fatalf("error init local output %v", err)
+	}
+
+	results, err := cr.CollectServiceLogs("kubelet")
+	if err != nil {
+		t.Fatalf("error in collecting %v", err)
+	}
+
+	if len(results) == 0 {
+		t.Fatalf("alteast one service exists but not found")
+	}
+	t.Log(results)
+
+	os.RemoveAll(dir)
+}
